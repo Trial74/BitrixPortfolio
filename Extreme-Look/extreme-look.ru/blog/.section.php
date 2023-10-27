e1e913907b3dc39b72d5c4f2b22d0bfbb9d1577a
Trial <51761394+Trial74@users.noreply.github.com>
2023-10-28 00:39:29 +0500

    Раздел с блогом

---
+++ Extreme-Look/extreme-look.ru/blog/.section.php
@@ -0,0 +1,10 @@
+<?
+$sSectionName = "Блог";
+$arDirProperties = Array(
+    "description" => "Статьи и интересные факты",
+    "keywords" => "extreme, look, экстрим, лук, статьи, факты, новости, информация",
+    "title" => "Блог",
+    "keywords_inner" => "extreme, look, экстрим, лук, статьи, факты, новости, информация",
+    "ROBOTS" => "index, follow"
+);
+?>
---
+++ Extreme-Look/extreme-look.ru/blog/video/.section.php
@@ -0,0 +1,10 @@
+<?
+$sSectionName = "Наши видео";
+$arDirProperties = Array(
+    "description" => "Наши видео от магазина Extreme Look",
+    "keywords" => "extreme, look, экстрим, лук, видео, материалы",
+    "title" => "Наши видео",
+    "keywords_inner" => "extreme, look, экстрим, лук, видео, материалы",
+    "ROBOTS" => "index, follow"
+);
+?>
