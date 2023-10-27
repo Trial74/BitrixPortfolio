d5959c5f312f2e0e67c3388e906c12a626e8ced8
Trial <51761394+Trial74@users.noreply.github.com>
2023-10-28 00:47:32 +0500

    Раздел с Live видео

---
+++ Extreme-Look/extreme-look.ru/live/.section.php
@@ -0,0 +1,10 @@
+<?
+$sSectionName = "Live Блог";
+$arDirProperties = Array(
+    "description" => "Live Блог",
+    "keywords" => "extreme, look, экстрим, лук, статьи, новости, информация",
+    "title" => "Live Блог",
+    "keywords_inner" => "extreme, look, экстрим, лук, статьи, новости, информация",
+    "ROBOTS" => "index, follow"
+);
+?>
