9ba994de7b135f85f67539fe96c8dd25970617b1
Trial <51761394+Trial74@users.noreply.github.com>
2023-10-28 01:32:42 +0500

    Загрузчик классов

---
+++ Extreme-Look/php_interface/autoload.php
@@ -0,0 +1,10 @@
+<?
+Bitrix\Main\Loader::registerAutoLoadClasses(null, [
+    'VladClasses\UriPartClassRoute'     => '/bitrix/php_interface/lib/partRoutControllerClass.php',
+    'VladClasses\ImageLibrary'          => '/bitrix/php_interface/lib/ImgLibClass.php',
+    'VladClasses\pushTokenAppClass'     => '/bitrix/php_interface/lib/pushTokenAppClass.php',
+    'VladClasses\qrCodeGeneratedLib'    => '/bitrix/php_interface/lib/qrCodeLib/qrlib.php',
+    'VladClasses\ImportPartXML'         => '/bitrix/php_interface/lib/ImportPartXML.php',
+    'VladClasses\AppBasket'             => '/bitrix/php_interface/lib/AppBasket.php',
+    'VladClasses\extremeCronClass'      => '/bitrix/php_interface/lib/extremeCronClass.php'
+]);
