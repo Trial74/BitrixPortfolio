da1b11cfc503ceaad7100b27107fb894ac669cbe
Trial <51761394+Trial74@users.noreply.github.com>
2023-10-27 23:53:17 +0500

    Просмотрщик логов

---
+++ Extreme-Look/admin_module/logs.php
@@ -0,0 +1,60 @@
+<?require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+IncludeModuleLangFile(__FILE__);
+$APPLICATION->SetTitle('Логи - функция vlog()');
+if(!$USER->IsAdmin() || !$USER->GetID() == 10354){
+    $APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
+}
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_after.php");?>
+<table style="margin:10px 0">
+    <tr>
+        <td><button onclick="reloadData()">Перезагрузить</button></td>
+        <td><button onclick="remDataFile()">Очистить файл</button></td>
+    </tr>
+    <tr>
+        <td colspan="2" style="border-bottom: 2px solid gray"></td>
+    </tr>
+</table>
+<div id="file_log_block"></div>
+<script>
+    BX.ready(function () {
+        var arrData = {};
+        arrData['action'] = 'loadFile';
+        BX.ajax.post(
+            '/bitrix/admin/extremelook_log_dump_ajax.php',
+            arrData,
+            function (data) {
+                var resultOBJ = JSON.parse(data);
+                if(!data.error) BX.adjust(BX('file_log_block'), {html: '<pre>' + resultOBJ.result + '</pre>'});
+                else BX.adjust(BX('file_log_block'), {text: 'Ошибка'});
+            }
+        );
+    });
+    function remDataFile() {
+        var arrData = {};
+        arrData['action'] = 'removeData';
+        BX.ajax.post(
+            '/bitrix/admin/extremelook_log_dump_ajax.php',
+            arrData,
+            function (data) {
+                var resultOBJ = JSON.parse(data);
+                if(!resultOBJ.error) BX.adjust(BX('file_log_block'), {text: 'Файл очищен'});
+                else BX.adjust(BX('file_log_block'), {text: 'Ошибка'});
+            }
+        );
+    }
+    function reloadData() {
+        var arrData = {};
+        arrData['action'] = 'loadFile';
+        BX.ajax.post(
+            '/bitrix/admin/extremelook_log_dump_ajax.php',
+            arrData,
+            function (data) {
+                var resultOBJ = JSON.parse(data);
+                if(!resultOBJ.error) BX.adjust(BX('file_log_block'), {html: '<pre>' + resultOBJ.result + '</pre>'});
+                else BX.adjust(BX('file_log_block'), {text: 'Ошибка'});
+            }
+        );
+    }
+</script>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_admin.php");?>
